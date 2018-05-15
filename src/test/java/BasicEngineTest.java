2f99eb622f6fe290a580b75e7823c710d1b2f27a
WMIC\ianma <ianma@wawanesa.com>
2018-05-15 14:15:36 -0500

    jenkins ready

--- src/test/java/BasicEngineTest.java
+++ src/test/java/BasicEngineTest.java
@@ -16,59 +16,37 @@ public class BasicEngineTest {
         this.engine = new BasicEngine(new EventFiringWebDriver(new ChromeDriver()), new EventL());
     }
 
-    @Test
-    public void page() {
-    }
-
-    @Test
-    public void page1() {
-    }
-
-    @Test
-    public void element() {
-    }
-
-    @Test
-    public void waitForElement() {
-    }
-
-    @Test
-    public void click() {
-    }
-
-    @Test
-    public void enter() {
-    }
-
-    @Test
-    public void close() {
-    }
-
-    @Test
-    public void quit() {
-    }
-
-    @Test
-    public void basicTest() throws IOException {
-        engine.page("https://www.google.ca", "google home")
-                .element("//*[@id=\"gb_70\"]")
-                .click()
-                .waitForElement(10L, "//*[@id=\"identifierId\"]")
-                .takesScreenshot()
-                .sendValue(username)
-                .sendEnter()
-                .waitForElement(10L, "//*[@id=\"password\"]/div[1]/div/div[1]/input")
-                .sendValue(password)
-                .takesScreenshot()
-                .sendEnter()
-                .waitForElement(10L, "//*[@id=\"gsr\"]")
-                .click()
-                .waitForElement(10L, "//*[@id=\"gb_71\"]")
-                .click()
-                .pause(10)
-                .close()
-                .quit();
-    }
+//    @Test
+//    public void page() {
+//    }
+//
+//    @Test
+//    public void page1() {
+//    }
+//
+//    @Test
+//    public void element() {
+//    }
+//
+//    @Test
+//    public void waitForElement() {
+//    }
+//
+//    @Test
+//    public void click() {
+//    }
+//
+//    @Test
+//    public void enter() {
+//    }
+//
+//    @Test
+//    public void close() {
+//    }
+//
+//    @Test
+//    public void quit() {
+//    }
 
     @Test
     public void confluenceTest() throws IOException {
